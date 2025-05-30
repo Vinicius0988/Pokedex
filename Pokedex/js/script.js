e0c217e51f5ef921e44364c5204538c812a8d7a5
Vinicius_siqueira098 <viniv908@gmail.com>
2025-05-29 23:03:35 -0300

    Add files via upload

---
+++ Pokedex/js/script.js
@@ -0,0 +1,35 @@
+const pokemonName = document.querySelector('.pokemon__name')
+const pokemonNumber = document.querySelector('.pokemon__number')
+const pokemonImage = document.querySelector('.pokemon__image')
+const form = document.querySelector('.form')
+const input = document.querySelector('.input__search')
+
+const fetchPokemon = async (Pokemon) => {
+
+    const APIResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${Pokemon.toLowerCase()}`)
+
+    if (APIResponse.status == 200){
+
+    const data = await APIResponse.json()
+    return data
+    }
+}
+
+const renderPokemon = async (pokemon) => {
+
+    const data = await fetchPokemon(pokemon)
+
+    pokemonName.innerHTML = data.name
+    pokemonNumber.innerHTML = data.id
+    pokemonImage.src = data['sprites']['versions']['generation-v']['black-white']['animated']['front_default']
+
+    input.value = ''
+}
+
+
+form.addEventListener('submit', (event) => {
+    event.preventDefault()
+
+    renderPokemon(input.value)
+})
+
